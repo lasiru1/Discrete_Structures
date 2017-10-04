4d09c512cf1dad41d8befc783ec809368a54f846
Uriel Salazar <usalazar1@student.rccd.edu>
2017-10-04 16:06:48 -0700

    Assignment 12

--- Assignment 11/src/GreatestCommonDivisor.java
+++ Assignment 11/src/GreatestCommonDivisor.java
@@ -8,17 +8,16 @@
 
 //import packages
 import java.util.Scanner;
-import java.awt.*;
 
 //declare class
 public class GreatestCommonDivisor
 {
-    //declare method "getMinimum"
-    public static int getGCD(int n1, int n2)
+    //declare method "getBruteForceGCD"
+    public static long getBruteForceGCD(long n1, long n2)
     {
         //declare & initialize variables
-        int min = 0;
-        int divisor = 0;
+        long min = 0;
+        long divisor = 0;
 
         //determine the lesser value between the two inputs
         min = Math.min(n1, n2);
@@ -35,16 +34,36 @@ public class GreatestCommonDivisor
         return divisor;
     }
 
+    //declare method "euclidAlgorithm"
+    public static long euclidAlgorithm(long a, long b)
+    {
+        //declare variables
+        long min = 0;
+
+        //determine the lesser value between the two inputs
+        min = Math.min(a, b);
+
+        if(min == 0)
+        {
+            return a;
+        }
+        else
+        {
+            return euclidAlgorithm(b, (a % b));
+        }
+    }
+
     //declare main program method
     public static void main(String[] args)
     {
         //declare & initialize variables
-        String temp1 = "";
-        String temp2 = "";
-        int num1 = 0;
-        int num2 = 0;
-        int numMin = 0;
-        int gcd = 0;
+        String      temp1 = "";
+        String      temp2 = "";
+        long        num1 = 0;
+        long        num2 = 0;
+        long        gcd = 0;
+        long  startTime = 0;
+        long  duration = 0;
 
         //declare objects
         Scanner input = new Scanner(System.in);
@@ -62,7 +81,7 @@ public class GreatestCommonDivisor
                 //convert string to integer
                 try
                 {
-                    num1 = Integer.parseInt(temp1);
+                    num1 = Long.parseLong(temp1);
                 }
                 catch (NumberFormatException e)
                 {
@@ -89,7 +108,7 @@ public class GreatestCommonDivisor
                 //convert string to integer
                 try
                 {
-                    num2 = Integer.parseInt(temp2);
+                    num2 = Long.parseLong(temp2);
                 }
                 catch (NumberFormatException e)
                 {
@@ -103,14 +122,40 @@ public class GreatestCommonDivisor
             }
         }while(temp2.matches("^[1-9]\\d*$") == false);
 
-        //call method "findGCD"
-        gcd = getGCD(num1, num2);
+        //start recording computation time
+        startTime = System.nanoTime();
+
+        //call method "getBruteForceGCD"
+        gcd = getBruteForceGCD(num1, num2);
+
+        //get total computation time
+        duration = System.nanoTime() - startTime;
 
         //print results
-        System.out.print("Input: " + num1 + ", " + num2 + "\n");
-        System.out.print("\nThe greatest common divisor is: " + gcd);
+        System.out.print("Input: " + num1 + ", " + num2 + "\n\n");
+        System.out.print("\nUSING BRUTE FORCE\n-----------------" +
+                "\nThe greatest common divisor is: " + gcd + "\n");
+        System.out.println("Total computation time: " + duration + " ns\n\n");
+
+        //start recording computation time
+        startTime = duration = 0;
+        startTime = System.nanoTime();
+
+        //call method "euclidAlgorithm"
+        gcd = euclidAlgorithm(num1, num2);
+
+        //get total computation time
+        duration = System.nanoTime() - startTime;
+
+        //print results
+        System.out.print("\nUSING EUCLID'S ALGORITHM" +
+                "\n------------------------" + "\nThe greatest common divisor" +
+                "is: " + gcd + "\n");
+        System.out.println("Total computation time: " + duration + " ns\n\n");
 
         //close scanner object
         input.close();
+
+
     }
 }
